897e6c158d42347e87b40c1a4e13430c21db99c8
Benson Chen <benson.chen.1@stonybrook.edu>
2024-02-13 21:25:35 -0500

    made the games tab prettier but still need to add all teams

--- Frontend/MainArea/Games/GameCard.js
+++ Frontend/MainArea/Games/GameCard.js
@@ -1,39 +1,79 @@
-import { View, Text, Image } from "react-native"
+import { useState } from "react"
+import { View, Text, Image, TouchableOpacity } from "react-native"
 import { getTeamLogo } from "../../../assets/TeamLogos/getTeamLogo"
+import { playBtnColor } from "../../../assets/Themes/ThemeColors"
 import { ThemeFonts } from "../../../assets/Themes/ThemeFont"
 
 export const GameCard = ({game}) => {
+
+    const [pickedTeam, setPickedTeam] = useState(null);
+
     return(
-        <View style={{width: "95%", backgroundColor:'white', borderRadius: 10}}>
+        <View style={{
+            width: "95%", backgroundColor:'white',height:250, borderRadius: 10, 
+            shadowColor:'white', shadowOffset: { width: 5, height: 5 }, shadowRadius:5, elevation: 5,
+        }}>
 
-            <View style={{width:"100%", flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
-                <View style={{alignItems:'center', marginLeft:10, marginTop:10}}>
-                    <TeamPicAndName team={game.homeTeam} />
-                    <Text style={{color:'grey'}}>Home</Text>
-                </View>
+            <View style={{width:"100%", flexDirection:'row', alignItems:'center'}}>
+                <TeamPicAndStats 
+                    team={game.homeTeam} isHome={true}
+                    pickedTeam={pickedTeam} setPickedTeam={setPickedTeam} 
+                />
 
-                <View style={{flexDirection:'row', alignItems:'center'}}>
+                <View style={{alignItems:'center', width:"30%"}}>
                     <Text style={{fontFamily:"Roboto-Black", fontSize:25, color:'rgba(0,0,0,1)'}}>7:30</Text>
                     <Text style={{fontFamily:"Roboto-Black", fontSize:20, marginLeft:5, color:'rgba(0,0,0,.5)'}}>PM EST</Text>
                 </View>
 
-                <View style={{alignItems:'center', marginRight:10, marginTop:10}}>
-                    <TeamPicAndName team={game.awayTeam} />
-                    <Text style={{color:'grey'}}>Away</Text>
-                </View>
+                <TeamPicAndStats 
+                    team={game.awayTeam} isHome={false}
+                    pickedTeam={pickedTeam} setPickedTeam={setPickedTeam}
+                />
+            </View>
+
+            <View style={{width:"100%", height: 20, flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+                
+                {pickedTeam ?
+                    <TouchableOpacity style={{
+                        height:40, width:100, backgroundColor:playBtnColor(pickedTeam).shadow, borderRadius:10
+                    }}>
+                        <View style={{
+                            height: 38, width:"100%", backgroundColor: playBtnColor(pickedTeam).main, 
+                            borderRadius:10, alignItems:'center', justifyContent:'center'
+                        }}>
+                            <Text style={{color:"white", fontFamily:ThemeFonts, fontSize:20}}>Play</Text>
+                        </View>
+                    </TouchableOpacity> 
+                        :
+                    <Text style={{fontFamily:ThemeFonts, fontSize:20, color:'#292f29'}}>
+                        Pick A Team To Play
+                    </Text> 
+                }
             </View>
 
         </View>
     )
 }
 
-const TeamPicAndName = ({team}) => {
+const TeamPicAndStats = ({team, isHome, pickedTeam, setPickedTeam}) => {
     return(
-        <View style={{alignItems:'center'}}>
-            <Image source={getTeamLogo(team.teamName)} style={{
-                width:60, height:60
-            }}/>
-            <Text style={{color:"black", fontFamily:ThemeFonts, fontSize:18}}>{team.teamName}</Text>
-        </View>
+        <TouchableOpacity style={{
+            alignItems:'center', paddingTop:10, width: "35%", alignItems:'center', 
+            justifyContent:'center', height: "100%", 
+            opacity: pickedTeam === team.teamName ? 1 : .25
+        }} onPress={() => setPickedTeam(team.teamName)}>
+
+             <View style={{alignItems:'center'}}>
+                <Image source={getTeamLogo(team.teamName)} style={{
+                    width: pickedTeam === team.teamName ? 90 : 60, 
+                    height: pickedTeam === team.teamName ? 90 : 60, 
+                }}/>
+                <Text style={{color:"black", fontFamily:ThemeFonts, fontSize:18}}>{team.teamName}</Text>
+            </View>
+
+            <Text style={{color:'grey'}}>
+                {isHome ? "Home" : "Away"}
+            </Text>
+        </TouchableOpacity>
     )
 }
--- assets/Themes/ThemeColors.js
+++ assets/Themes/ThemeColors.js
@@ -1,3 +1,13 @@
 export const themeColors = {
     mainSelectedColor : "#8000FF"
 }
+
+export const playBtnColor = (name) => {
+    if(name === "Nets"){
+        return {main: '#000000', shadow: '#6b726b'}
+    } else if(name === "Celtics"){
+        return {main: '#12b411', shadow: '#007a34'}
+    } else {
+        return {main: 'orange', shadow: 'yellow'}
+    }
+}
